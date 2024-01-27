c6a40c8b11cc031032cdfa1fe389c94012670c2a
John Chrosniak <jlc9wr@virginia.edu>
2024-01-26 21:27:03 -0500

    add some pipeline files

---
+++ 01_bag_to_images.py
@@ -0,0 +1,63 @@
+from rosbags.rosbag2 import Reader
+from rosbags.serde import deserialize_cdr
+import tqdm
+import numpy as np
+import cv2
+import json
+import sys
+import os
+
+def extract(bag_dir, topic, output_dir):
+    timestamps = []
+    count = 0
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # Create reader instance and open for reading
+    with Reader(bag_dir) as reader:
+        # Iterate over messages
+        with tqdm.tqdm(desc='Reading images.') as pbar:
+            for connection, timestamp, rawdata in reader.messages():
+                # Check if the message topic matches the specified format
+                if connection.topic == topic:
+                    count += 1
+                    msg = deserialize_cdr(rawdata, connection.msgtype)
+                    np_arr = np.fromstring(msg.data, np.uint8)
+                    image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+                    image_np = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
+                    timestamps.append(timestamp)
+                    path = f'{output_dir}/{count}.png'
+                    if not os.path.exists(path):
+                        cv2.imwrite(path, image_np)
+                    pbar.update(1)
+    
+    # Also store a `timestamps.json` file.
+    with open(f'{output_dir}/timestamps.json', 'w') as f:
+        json.dump(timestamps, f)
+
+'''
+Topic: /vehicle_8/camera/front_left/camera_info | Type: sensor_msgs/msg/CameraInfo | Count: 16363 | Serialization Format: cdr
+Topic: /vehicle_8/camera/front_left/image/compressed | Type: sensor_msgs/msg/CompressedImage | Count: 3062 | Serialization Format: cdr
+Topic: /vehicle_8/camera/front_left_center/camera_info | Type: sensor_msgs/msg/CameraInfo | Count: 16503 | Serialization Format: cdr
+Topic: /vehicle_8/camera/front_left_center/image/compressed | Type: sensor_msgs/msg/CompressedImage | Count: 5092 | Serialization Format: cdr
+Topic: /vehicle_8/camera/front_right/camera_info | Type: sensor_msgs/msg/CameraInfo | Count: 16352 | Serialization Format: cdr
+Topic: /vehicle_8/camera/front_right/image/compressed | Type: sensor_msgs/msg/CompressedImage | Count: 4250 | Serialization Format: cdr
+Topic: /vehicle_8/camera/rear_left/camera_info | Type: sensor_msgs/msg/CameraInfo | Count: 16324 | Serialization Format: cdr
+Topic: /vehicle_8/camera/rear_left/image/compressed | Type: sensor_msgs/msg/CompressedImage | Count: 4803 | Serialization Format: cdr
+Topic: /vehicle_8/camera/rear_right/camera_info | Type: sensor_msgs/msg/CameraInfo | Count: 16309 | Serialization Format: cdr
+Topic: /vehicle_8/camera/rear_right/image/compressed
+'''
+
+def main():
+    if len(sys.argv) != 4:
+        print("Usage: python bag_to_images.py <bag_dir> <topic> <output_dir>")
+        exit(1)
+
+    bag_dir = sys.argv[1]
+    topic = sys.argv[2]
+    output_dir = sys.argv[3]
+    extract(bag_dir, topic, output_dir)
+
+if __name__ == '__main__':
+    main()
---
+++ 02_images_to_detections.py
@@ -0,0 +1,92 @@
+"""
+Usage: python detect_cars.py </path/to/images> <output_file>
+"""
+
+# Grounding DINO
+import groundingdino.datasets.transforms as T
+from groundingdino.models import build_model
+from groundingdino.util.slconfig import SLConfig
+from groundingdino.util.utils import clean_state_dict
+from groundingdino.util.inference import predict
+import glob
+import sys
+
+from huggingface_hub import hf_hub_download
+
+import torch
+import PIL.Image
+from typing import Tuple
+import tqdm
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def load_model_hf(repo_id, filename, ckpt_config_filename, device):
+    cache_config_file = hf_hub_download(repo_id=repo_id, filename=ckpt_config_filename)
+
+    args = SLConfig.fromfile(cache_config_file) 
+    model = build_model(args)
+    args.device = device
+
+    cache_file = hf_hub_download(repo_id=repo_id, filename=filename)
+    checkpoint = torch.load(cache_file, map_location=device)
+    log = model.load_state_dict(clean_state_dict(checkpoint['model']), strict=False)
+    print("Model loaded from {} \n => {}".format(cache_file, log))
+    _ = model.eval()
+    return model
+
+transform = T.Compose([
+    # Selects a "random" size from the list [800,]
+    T.RandomResize([800], max_size=1333),
+    T.ToTensor(),
+    T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+])
+
+DetectionResult = Tuple[torch.Tensor, torch.Tensor, list]
+
+# Infer on image folder
+def infer_image_folder(input_path: str, output_file: str):
+    # Use this command for evaluate the Grounding DINO model
+    # Or you can download the model by yourself
+    ckpt_repo_id = "ShilongLiu/GroundingDINO"
+    ckpt_filename = "groundingdino_swinb_cogcoor.pth"
+    ckpt_config_filename = "GroundingDINO_SwinB.cfg.py"
+
+    model = load_model_hf(ckpt_repo_id, ckpt_filename, ckpt_config_filename, device)
+    model = model.to(device)
+
+    # Detection settings
+    TEXT_PROMPT = "car"
+    BOX_TRESHOLD = 0.3
+    TEXT_TRESHOLD = 0.25
+
+    image_paths = sorted(list(glob.glob(f"{input_path}/*.png")) + list(glob.glob(f"{input_path}/*.jpg")))
+    results = []
+
+    for image_path in tqdm.tqdm(image_paths, desc='Detecting cars.'):
+        raw_image = PIL.Image.open(image_path).convert("RGB")
+        image, _ = transform(raw_image, None)
+        boxes, logits, phrases = predict(
+            model=model, 
+            image=image,
+            caption=TEXT_PROMPT, 
+            box_threshold=BOX_TRESHOLD, 
+            text_threshold=TEXT_TRESHOLD,
+            device=device,
+        )
+        results.append((boxes, logits, phrases))
+    
+    import pickle
+
+    with open(output_file, "wb") as f:
+        pickle.dump(results, f)
+    
+    print(f"Saved results to {output_file}.")
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("Usage: python detect_cars.py </path/to/images> <output_file>")
+        exit(1)
+
+    input_path = sys.argv[1]
+    output_file = sys.argv[2]
+    infer_image_folder(input_path, output_file)
