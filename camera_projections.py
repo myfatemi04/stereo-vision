2e9d39da7e2d90b20ed70ab2da52ba3f71ef1386
John Chrosniak <jlc9wr@virginia.edu>
2024-01-27 10:55:56 -0500

    lidar camera projections?

--- camera_projections.py
+++ camera_projections.py
@@ -65,7 +65,15 @@ with open("intrinsic_matrices.json", "r") as f:
     intrinsics = json.load(f)
     intrinsics = {k: np.array(v) for k, v in intrinsics.items()}
 
-def create_camera_matrix(extrinsics, intrinsics):
+def invert_extrinsics(extrinsics):
+    """
+    The extrinsic matrix is 3x4. Therefore, we can't simply take a matrix inverse.
+    We need to inver the rotation matrix and translation matrix separately.
+    Specifically, we:
+    (1) invert the rotation matrix, and
+    (2) multiply the translation matrix by the inverse rotation matrix, and
+    (3) invert the translation by negating the translation matrix
+    """
     # Get 3x3 rotation matrix
     R = extrinsics[:3, :3]
     # Get 3x1 translation matrix
@@ -75,34 +83,52 @@ def create_camera_matrix(extrinsics, intrinsics):
     R_inv = R.T
     R_inv_C = np.matmul(R_inv, C)
     # Create new matrix
-    extrinsics = np.concatenate((R_inv, -R_inv_C), -1)
-    # Multiply matrices to form camera projection matrix
-    cam_proj_mat = np.matmul(intrinsics, extrinsics)
-    return cam_proj_mat
+    return np.concatenate((R_inv, -R_inv_C), -1)
+
+def create_camera_matrix(extrinsics, intrinsics):
+    return np.matmul(intrinsics, invert_extrinsics(extrinsics))
 
 # Now, we should be able to convert from world space to camera space.
 def test_camera_projection():
     cameras_to_use = ['front_left', 'front_left_center', 'front_right']
     base_path = 'M-MULTI-SLOW-KAIST_images'
-    index = 1
+    index = 100
     X, Y, Z = np.meshgrid(np.linspace(0, 1, 8), np.linspace(0, 1, 8), np.linspace(0, 1, 8))
+
+    with open(f"M-MULTI-SLOW-KAIST_lidar/luminar_front_points/{index}.pkl", "rb") as f:
+        data = pickle.load(f)
+        X, Y, Z = data[0].T
+
+    plt.figure(figsize=(12, 4))
+    
     points = np.stack((X.flatten(), Y.flatten(), Z.flatten(), np.ones(X.flatten().shape[0])), axis=1)
     points[:, 0] += 10
     points[:, 1] += -0.5
 
     for i, camera in enumerate(cameras_to_use):
-        mat = create_camera_matrix(extrinsics[camera], intrinsics[camera])
-        point_camera_space = (mat @ points.T)
+        points_camera_frame_3d = np.matmul(invert_extrinsics(extrinsics[camera]), points.T).T
+        points_camera_plane_2d = np.matmul(intrinsics[camera], points_camera_frame_3d.T).T
+        depths = np.linalg.norm(points_camera_frame_3d, axis=1)
         # filter out points that end up behind the camera
-        point_camera_space = point_camera_space[:, point_camera_space[-1] > 0]
-        point_camera_space = (point_camera_space / point_camera_space[-1])
+        mask = points_camera_plane_2d[:, -1] > 0
+        points_camera_plane_2d = points_camera_plane_2d[mask]
+        depths = depths[mask]
+        points_camera_plane_2d = (points_camera_plane_2d / points_camera_plane_2d[:, [-1]])
+        # filter out points that are out of frame
+        x = points_camera_plane_2d[:, 0]
+        y = points_camera_plane_2d[:, 1]
+        mask = (0 < x) & (x < 2064) & (0 < y) & (y < 960)
+        points_camera_plane_2d = points_camera_plane_2d[mask]
+        depths = depths[mask]
 
         plt.subplot(1, 3, i + 1)
         plt.imshow(plt.imread(f"{base_path}/{camera}/{index}.png"))
         # if 0 < x < 2064 and 0 < y < 960:
-        plt.scatter(point_camera_space[0], point_camera_space[1])
+        plt.scatter(points_camera_plane_2d[:, 0], points_camera_plane_2d[:, 1], c=-1/depths, s=1, cmap='jet')
         plt.axis('off')
         plt.title(camera)
+        if i == len(cameras_to_use) - 1:
+            plt.colorbar()
 
     plt.savefig("camera_projections.png")
 
